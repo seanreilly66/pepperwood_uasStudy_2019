ea3c498b9fb4a27a5d675cadc26c9dd9b2e0b36f
Sean Reilly <sean.reilly66@gmail.com>
2020-09-14 14:36:39 -0700

    Initial commit

---
+++ R/chm_generation.R
@@ -0,0 +1,118 @@
+# ===============================================================================
+#
+# Canopy height model generation
+#
+# ===============================================================================
+#
+# Author: Sean Reilly, sean.reilly66@gmail.com
+#
+# Created: 16 Aug 2020
+# Last commit: 14 Sept 2020
+#
+# Status: Under development
+#
+# This file created as part of 2019 Pepperwood UAS study
+#
+# ===============================================================================
+#
+# Description:
+#
+# Generates canopy height models for UAS and ALS height normalized data using
+# the pitfree algorithm with default parameters. Performs a two pass kernel
+# smoothing filter to remove outliers.
+#
+# ===============================================================================
+# 
+# User inputs:
+#
+# zone = Vector of zone numbers
+# las_file = .las or .laz file name (.las faster operation) skeleton for height 
+#   normalized point cloud containing {pntcld_source}, {dtm_source}, and {z} zone
+#   glue placeholders.
+# chm_res = Canopy height model resolution (meters)
+# output = Output file name containing {pntcld_source}, {dtm_source}, and {z} zone
+#   glue placeholders
+# 
+# ===============================================================================
+# 
+# Package dependences: 
+#
+# sp, raster, lidR, glue
+# 
+# ===============================================================================
+#
+# Known problems:
+#
+# ===============================================================================
+
+# =============================== Load libraries ================================ 
+
+lib = 'r_lib' # for linux cluster use
+# lib = NULL # for local use
+
+suppressPackageStartupMessages(library(sp, lib.loc = lib))
+suppressPackageStartupMessages(library(raster, lib.loc = lib))
+suppressPackageStartupMessages(library(lazyeval, lib.loc = lib))
+suppressPackageStartupMessages(library(rlas, lib.loc = lib))
+suppressPackageStartupMessages(library(RCSF, lib.loc = lib))
+suppressPackageStartupMessages(library(lidR, lib.loc = lib))
+suppressPackageStartupMessages(library(rgdal, lib.loc = lib))
+suppressPackageStartupMessages(library(rgeos, lib.loc = lib))
+suppressPackageStartupMessages(library(glue, lib.loc = lib))
+
+rm(lib)
+
+# ================================= User inputs =================================
+
+zone <- c(2:4, 6:13)
+
+las_file <- 'data/las/{pntcld_source}/ppwd_{pntcld_source}_z{z}_f2_hnorm-{dtm_source}.las'
+
+chm_res <- 0.5
+
+output <- 'data/chm/ppwd_{pntcld_source}_z{z}_f2_hnorm-{dtm_source}_chm'
+
+# =========================== CHM generation function =========================== 
+
+chm_gen <- function(las_file, res = chm_res) {
+  
+  las <- readLAS(las_file, select = ('r')) 
+  
+  chm <- grid_canopy(las, res, pitfree(subcircle = 0.15))
+  
+  ker = matrix(1,3,3)
+
+  chm <- focal(chm, w = ker, fun = mean, na.rm = TRUE)
+  chm <- focal(chm, w = ker, fun = mean, na.rm = TRUE)
+  
+  return(chm)
+
+}
+
+# ============= Generate CHMs from combinations of uas and als data ============= 
+
+data_source <- c('uas', 'als')
+
+for (pntcld_source in data_source) {
+  for (dtm_source in data_source) {
+    
+    if (pntcld_source == 'als' & dtm_source == 'uas') {next}
+
+    message(pntcld_source, ' height normalized to ', dtm_source )
+    
+    for (z in zone) {
+      message('Processing zone: ',z)
+      
+      chm <- chm_gen(las_file = glue(las_file))
+
+      writeRaster(
+        x = chm,
+        filename = glue(output),
+        datatype='FLT4S',
+        format="GTiff",
+        overwrite=TRUE)
+    }
+  }
+}
+
+# ===============================================================================
