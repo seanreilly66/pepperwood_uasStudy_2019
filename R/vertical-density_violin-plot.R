6420b1ad3d2c689444d531451ad540c17a06e3e2
Sean Reilly <sean.reilly66@gmail.com>
2021-01-08 11:52:49 -0800

    Reformat code for readability

--- R/vertical-density_violin-plot.R
+++ R/vertical-density_violin-plot.R
@@ -1,8 +1,8 @@
-# ===============================================================================
+# ==============================================================================
 #
 # Vertical height density violin plot
 #
-# ===============================================================================
+# ==============================================================================
 #
 # Author: Sean Reilly, sean.reilly66@gmail.com
 #
@@ -13,15 +13,15 @@
 #
 # This file created as part of 2019 Pepperwood UAS study
 #
-# ===============================================================================
+# ==============================================================================
 #
 # Description:
 #
-# Generates violin plot of uas and als height kernal density by forest vegetation
+# Generates violin plot of uas and als height kernel density by forest vegetation
 # type from input height datasets
 #
-# ===============================================================================
-# 
+# ==============================================================================
+#
 # User inputs:
 #
 # decid_uas = Deciduous UAS height data csv file
@@ -30,36 +30,42 @@
 # evrgrn_als = Evergreen ALS height data csv file
 # conifer_uas = conifer UAS height data csv file
 # conifer_als = conifer ALS height data csv file
-# 
-# ===============================================================================
-# 
-# Package dependences: 
+#
+# ==============================================================================
+#
+# Package dependences:
 #
 # tidyverse, ggplot2, glue, ggpubr
-# 
-# ===============================================================================
+#
+# ==============================================================================
 #
 # Known problems:
 #
-# ===============================================================================
+# ==============================================================================
 
 library(tidyverse)
 library(ggplot2)
 library(glue)
 library(ggpubr)
 
-# ================================= User inputs =================================
+# ================================= User inputs ================================
 
-decid_uas <- 'data/pntcld_vertical_density/ppwd_uas_height_deciduous_unburned-forest-stand.csv'
-decid_als <- 'data/pntcld_vertical_density/ppwd_als_height_deciduous_unburned-forest-stand.csv'
+decid_uas <-
+  'data/pntcld_vertical_density/ppwd_uas_height_deciduous_unburned-forest-stand.csv'
+decid_als <-
+  'data/pntcld_vertical_density/ppwd_als_height_deciduous_unburned-forest-stand.csv'
 
-evrgrn_uas <- 'data/pntcld_vertical_density/ppwd_uas_height_evergreen_unburned-forest-stand.csv'
-evrgrn_als <- 'data/pntcld_vertical_density/ppwd_als_height_evergreen_unburned-forest-stand.csv'
+evrgrn_uas <-
+  'data/pntcld_vertical_density/ppwd_uas_height_evergreen_unburned-forest-stand.csv'
+evrgrn_als <-
+  'data/pntcld_vertical_density/ppwd_als_height_evergreen_unburned-forest-stand.csv'
 
-conifer_uas <- 'data/pntcld_vertical_density/ppwd_uas_height_conifer_unburned-forest-stand.csv'
-conifer_als <- 'data/pntcld_vertical_density/ppwd_als_height_conifer_unburned-forest-stand.csv'
+conifer_uas <-
+  'data/pntcld_vertical_density/ppwd_uas_height_conifer_unburned-forest-stand.csv'
+conifer_als <-
+  'data/pntcld_vertical_density/ppwd_als_height_conifer_unburned-forest-stand.csv'
 
-# ============================= Dataset combination ============================= 
+# ============================= Dataset combination ============================
 
 height_df <- bind_rows(
   read.csv(decid_uas) %>%
@@ -91,61 +97,62 @@ height_df <- bind_rows(
     dplyr::select(2) %>%
     rename(Z = 1) %>%
     add_column(veg_class = 'Conifer\nforest') %>%
-    add_column(data_type = 'ALS'))
+    add_column(data_type = 'ALS')
+)
 
-rm(decid_uas, decid_als, evrgrn_uas, evrgrn_als, conifer_uas, conifer_als)
+rm(decid_uas,
+   decid_als,
+   evrgrn_uas,
+   evrgrn_als,
+   conifer_uas,
+   conifer_als)
 
 height_df <- height_df %>%
   add_row(height_df %>%
             mutate(veg_class = 'All forests')) %>%
-    mutate_at(c('veg_class', 'data_type'), as.factor) %>%
+  mutate_at(c('veg_class', 'data_type'), as.factor) %>%
   mutate(
-    veg_class = fct_relevel(veg_class,
-      'All forests',
+    veg_class = fct_relevel(
+      veg_class,
       'Conifer\nforest',
       'Evergreen\nbroadleaf\nforest',
-      'Deciduous\nbroadleaf\nforest'
+      'Deciduous\nbroadleaf\nforest',
+      'All forests'
     )
   )
 
-# ============================== ggplot theme set =============================== 
+# ============================== ggplot theme set ==============================
 
 theme_set(
-  theme(text = element_text(family = 'serif', face = 'plain'),
-        axis.title = element_text(size = 16),
-        axis.text = element_text(size = 14),
-        line = element_line(size = 1),
-        axis.line = element_line(),
-        panel.background = element_rect(color = 'white'),
-        legend.title = element_text(size = 16),
-        legend.text = element_text(size = 14),
-        legend.key = element_blank(),
-        legend.spacing = unit(0, "cm"),
-        legend.margin = margin(0,5,0,5)
+  theme(
+    text = element_text(family = 'serif', face = 'plain'),
+    axis.title = element_text(size = 16),
+    axis.text = element_text(size = 14),
+    line = element_line(size = 1),
+    axis.line = element_line(),
+    panel.background = element_rect(color = 'white'),
+    legend.title = element_text(size = 16),
+    legend.text = element_text(size = 14),
+    legend.key = element_blank(),
+    legend.spacing = unit(0, "cm"),
+    legend.margin = margin(0, 5, 0, 5)
   )
 )
 
-# ============================ Generate violin plot ============================= 
+# ============================ Generate violin plot =============================
 
-fig <- ggplot(
-  data = height_df,
-  mapping = aes(
-    x = veg_class,
-    y = Z,
-    fill = data_type)) + 
-  geom_violin(
-    draw_quantiles = c(0.25, 0.5, 0.75)) +
-  labs(
-    y = 'Height (m)',
-    x = NULL) +
-  scale_y_continuous(
-    limits = c(0,50)
-    ) +
-  scale_fill_grey(
-    start = 1,
-    end = 0.8,
-    name = NULL) +
-  theme(legend.position = c(0.8, 0.8))
+fig <- ggplot(data = height_df,
+              mapping = aes(x = veg_class,
+                            y = Z,
+                            fill = data_type)) +
+  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
+  labs(y = 'Height (m)',
+       x = NULL) +
+  scale_y_continuous(limits = c(0, 50)) +
+  scale_fill_grey(start = 1,
+                  end = 0.8,
+                  name = NULL) +
+  theme(legend.position = c(0.5, 0.8))
 
 fig
 
@@ -154,6 +161,7 @@ ggsave(
   width = 5.5,
   height = 3.5,
   units = 'in',
-  dpi = 700)
+  dpi = 700
+)
 
-# ===============================================================================
+# ==============================================================================
